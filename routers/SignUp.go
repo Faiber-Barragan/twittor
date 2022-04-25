122870151e3b74e2359949ba6bb8ff9926987c3d
Faiber-Barragan <barragan.faiber@gmail.com>
2022-04-24 19:54:48 -0500

    fix nil pointer err.Error at routers

--- routers/SignUp.go
+++ routers/SignUp.go
@@ -2,9 +2,10 @@ package routers
 
 import (
 	"encoding/json"
+	"net/http"
+
 	"github.com/Faiber-Barragan/twittor/db"
 	"github.com/Faiber-Barragan/twittor/models"
-	"net/http"
 )
 
 //SignUp is the function to create in the DB the register of user
@@ -18,18 +19,18 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if len(t.Email) == 0 {
-		http.Error(w, "User Email is required: "+err.Error(), http.StatusBadRequest)
+		http.Error(w, "User Email is required", http.StatusBadRequest)
 		return
 	}
 
 	if len(t.Password) < 6 {
-		http.Error(w, "Password must be at least 6 characters: "+err.Error(), http.StatusBadRequest)
+		http.Error(w, "Password must be at least 6 characters", http.StatusBadRequest)
 		return
 	}
 
 	_, found, _ := db.CheckUserAlreadyExists(t.Email)
 	if found {
-		http.Error(w, "Already exists user sign up with that email: "+err.Error(), http.StatusBadRequest)
+		http.Error(w, "Already exists user sign up with that email", http.StatusBadRequest)
 		return
 	}
 
@@ -40,7 +41,7 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if !status {
-		http.Error(w, "Failed to insert user record: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Failed to insert user record", http.StatusInternalServerError)
 		return
 	}
 
